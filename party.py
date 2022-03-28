9eb72c49518a02ca143fc1635859ef9b8686d83a
juan720v <Juan.Vela@mail.citytech.cuny.edu>
2022-03-28 16:10:12 -0400

    party.py format

---
+++ party.py
@@ -0,0 +1,11 @@
+partyCountDown = input("How long until the party starts? ")
+partyCountDown = int(partyCountDown)
+
+if partyCountDown < 1:
+    print("PARTY NOW!")
+else:
+    for i in range(partyCountDown, 0, -1):
+        print(i)
+        if i == 1:
+            print("PARTY TIME!")
+            
